f162c15a5578c13c64e880a3981731b5e3e2b5a6
Adam Black <ablack3@gmail.com>
2017-04-05 12:18:59 -0400

    Changed e-code mechanisms and intent to from numeric codes to character strings. Removed lowmech variable from output.

--- R/trauma.R
+++ R/trauma.R
@@ -60,63 +60,6 @@
 #'       \item  9 = Unknown
 #'}
 #'
-#' Codes for ISS body region:
-#' \itemize{
-#' \item 1 = Head/neck
-#' \item 2 = Face
-#' \item 3 = Chest
-#' \item 4 = Abdomen and pelvic contents
-#' \item 5 = Extremities or pelvic girdle
-#' \item 6 = External
-#' \item 9 = Unknown
-#'}
-#'  Codes for CDC external cause of injury major/minor mechanism categories:
-#' \itemize{
-#' \item 0 = Cut/pierce
-#' \item 1 = Drowning/submersion
-#' \item 2 = Fall
-#' \item 3 = Fire/burn
-#' \itemize{
-#'    \item Fire/flame = 0
-#'    \item Hot object/substance = 1
-#' }
-#' \item 4 = Firearm
-#' \item 5 = Machinery
-#' \item 6 = Motor vehicle traffic
-#'      \itemize{
-#'      \item Occupant = 0
-#'      \item Motorcyclist = 1
-#'      \item Pedal cyclist = 2
-#'      \item Pedestrian = 3
-#'      \item Unspecified = 4
-#'      }
-#' \item 7 = Pedal cyclist, other
-#' \item 8 = Pedestrian, other
-#' \item 9 = Transport, other
-#' \item 10 = Natural/environmental
-#'      \itemize{ \item Bites and stings = 0}
-#' \item 11 = Overexertion
-#' \item 12 = Poisoning
-#' \item 13 = Struck by, against
-#' \item 14 = Suffocation
-#' \item 15 = Other specified, classifiable
-#' \item 16 = Other specified, not elsewhere classifiable
-#' \item 17 = Unspecified
-#' \item 18 = Adverse effects
-#'      \itemize{
-#'      Medical care = 0
-#'      Drugs = 1
-#'      }
-#'}
-#' Codes for CDC external cause of injury intent category:
-#' \itemize{
-#' \item 0 = Unintentional
-#' \item 1 = Self-inflicted
-#' \item 2 = Assault
-#' \item 3 = Undetermined
-#' \item 4 = Other
-#' }
-#'
 #' @examples df_in <- read.table(header = T, text = "
 #' ident    dx1     dx2     dx3
 #' 31416   800.1   959.9   E910.9
@@ -411,9 +354,6 @@ trauma <- function(df, dx_pre, calc_method = 1, icd10 = TRUE, conflict_resolutio
           df <- .insert_columns(df,col_name,temp)
 
       }
-
-      # Create lowmech variable to hold the lowest major E-code mechanism code. #
-      df$lowmech <- pmin(df$mechmaj1, df$mechmaj2, df$mechmaj3, df$mechmaj4, na.rm = T)
 
       # Create bluntpen variable to hold type of trauma: blunt or penetrating. #
       df$bluntpen = NA
