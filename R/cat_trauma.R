4c50f7252ae5333ab2bcc0a7ab6198babc73e620
Adam Black <ablack3@gmail.com>
2017-08-09 11:34:29 -0400

    troubleshooted icd 10 code parsing. Package now depends on stringr.

--- R/cat_trauma.R
+++ R/cat_trauma.R
@@ -4,7 +4,7 @@
 #' \enumerate{
 #'    \item assign a severity (AIS) and ISS body region values to each valid ICD-9-CM or ICD 10 trauma code,
 #'    \item add variables for maximum severity of each body region,
-#'    \item calculate ISS and new ISS,
+#'    \item calculate ISS and new ISS
 #'    \item select first 4 e-codes/mechanism codes along with major mechanism, minor mechanism, and intent
 #'    \item summarize mechanism with lowest major mechanism and assign trauma type (blunt or penetrating) the first E-Code found.
 #'}
@@ -20,11 +20,19 @@
 #'          Method 2 will change any AIS = 6 to 5 and then calculate ISS normally.
 #' @param conflict_resolution Method for resolving ISS score conflicts when mapping ICD-10 codes to ICD 9
 #'          codes to AIS.
-#'          Must be either "max" or "min". Few ICD10 codes result in a conflict.
-#' @param icd10 A logical value indicating whether ICD 10 codes should be considered or ignored.
-#'          If FALSE then ICD10 codes are ignored.
-#'          If TRUE then mapped to ICD 9 using CMS's general equivalence mapping (GEM) and then
-#'          included in the calcuation of the ISS and ICD10 mechanism codes will be included in E-code calculation.
+#'          Must be either "max" or "min". Few ICD-10 codes result in a conflict.
+#' @param icd10 A logical value indicating whether ICD-10 codes should be considered or ignored.
+#'          If FALSE then ICD-10 codes are ignored.
+#'          If TRUE then ICD-10 codes are handled based on the i10_iss_method argument
+#'          and ICD10 mechanism codes will be included in E-code calculation.
+#' @param i10_iss_method Method for calculating ISS from ICD10-CM codes. Must be one of:
+#'          \itemize{
+#'          \item "empirical" Table derived empirically from _____ described in detail in ____ (default)
+#'          \item "gem_max" Table derived by mapping ICD 10 to ICD 9 using the CMS general equivalence mapping tables and then to ISS
+#'                 using the original ICDPIC table. Mapping conflicts handled by taking the max ISS.
+#'          \item "gem_min" Same as "gem_max" except that mapping conflicts are handled by taking the min ISS.
+#'          }
+#'
 #' @return A dataframe identical to the dataframe passed to the function with the following additional variables
 #'          added:
 #'          \itemize{
@@ -73,58 +81,77 @@
 #-----------------------------------------------------------------------------------------------------------------#
 #  For each observation, program to assign severity and ISS body region values to each valid ICD-9-CM trauma      #
 #  code, assign Barell and AP component categories to each valid ICD-9-CM trauma code, calculate injury severity  #
-#  score (ISS) and new injury severity score (NISS), assign major mechanism, minor mechanism and intent for up    #
+#  score (ISS), assign major mechanism, minor mechanism and intent for up                                         #
 #  to 4 E-Codes (excluding E-Code place) and assign trauma type (blunt or penetrating) based on major mechanism   #
 #  of the first E-Code found.                                                                                     #
 #-----------------------------------------------------------------------------------------------------------------#
-# dx_pre="DX"
-# calc_method = 1
 
-cat_trauma <- function(df, dx_pre, calc_method = 1, icd10 = TRUE, conflict_resolution = "max", i10_sev_method = "empirical"){
+# testing
+# set.seed(1)
+# codes <- c()
+# n <- 5
+# df <- data.frame(dx1 = sample(ntab_s1$dx, n),
+#                  dx2 = sample(ntab_s1$dx, n),
+#                  dx3 = sample(i10_map_emp$dx, n),
+#                  dx4 = sample(etab_s1$dx, n),
+#                  dx5 = sample(i10_ecode$dx, n))
+#
+# result <- cat_trauma(df,"dx")
+#
+# df
+# dx_pre="dx"
+# calc_method = 1
+# icd10 <- T
+# i10_iss_method <- "empirical"
+
+cat_trauma <- function(df, dx_pre, calc_method = 1, icd10 = TRUE, i10_iss_method = "empirical"){
 
       # Verify input #
       if(!is.data.frame(df)) stop("First argument must be a dataframe")
-      if(!is.character(dx_pre)) stop("Second argument must be a character string")
-      #also need to ensure dx_pre is a valid variable name too
-      if(!(calc_method %in% c(1,2))) stop("calc_method must be either 1 or 2")
       if(NROW(df) == 0) stop("Data contains no observations.")
-      if(!(conflict_resolution %in% c("max","min"))) stop("conflict resolution method must be either max or min")
+      if(!is.character(dx_pre)) stop("Second argument must be a character string")
+      # ensure dx_pre is a valid variable name
+      if(make.names(dx_pre) != dx_pre) stop("Second argument must be a valid variable name in R")
+      if(!(calc_method %in% c(1,2))) stop("calc_method must be either 1 or 2")
+      if(!(icd10 %in% c(T, F))) stop("icd10 must be TRUE or FALSE")
+      if(!(i10_iss_method %in% c("empirical","gem_max","gem_min"))) stop("i10_iss_menthod must be empirical, gem_max, or gem_min")
 
-
-      # Check if user entered a correct prefix for the diagnosis code variables in the input file.
-      # and Determine how many diagnosis code variables there are in the data. #
-      regex_dx <- paste(dx_pre,"([0-9]+)",sep="")
+      # Check if user entered a correct prefix for the diagnosis code variables in the input file
+      # Determine how many diagnosis code variables there are in the data
+      regex_dx <- paste0("^", dx_pre, "([0-9]+)$")
       dx_colnames <- grep(regex_dx, names(df), value = T)
-      dx_nums <- as.numeric(sub(regex_dx,"\\1",dx_colnames ))
+      # replace full column name with first capture group and convert to number
+      dx_nums <- as.numeric(sub(regex_dx, "\\1", dx_colnames))
       num_dx <- length(dx_nums)
       if(num_dx == 0) stop("No variables with prefix found in data")
 
+      # make sure df is not a tibble and if it is convert back to dataframe
+      df <- data.frame(df)
 
       # add i10 codes to lookup tables
       # The i10 mappings  for n codes were created by using CMS general equivalence mappings
       # The i10 maapings for e codes (mechanism) were created using CDC injury mechanism grid
       # See documentation and prelim directory for details
       if(icd10){
-            etab_s1 <- rbind(etab_s1, i10_ecode)
+            etab <- rbind(etab_s1, i10_ecode)
 
-            if(conflict_resolution == "max"){
-                  ntab_s1 <- rbind(ntab_s1, i10_map_max)
+            if(i10_iss_method == "empirical"){
+                  ntab <- rbind(ntab_s1, i10_map_emp)
+            } else if(i10_iss_method == "gem_max"){
+                  ntab <- rbind(ntab_s1, i10_map_max)
+            } else if(i10_iss_method == "gem_min"){
+                  ntab <- rbind(ntab_s1, i10_map_min)
             }
-            else{
-                  ntab_s1 <- rbind(ntab_s1, i10_map_min)
-            }
+      } else {
+            ntab <- ntab_s1
+            etab <- etab_s1
       }
-
-
-      # Create temporary variable to count the number of diagnosis codes with both an
-      # unknown severity and an unknown  ISS body region.
-      df$unk_unk <- 0
 
       #---------------------------------------------------------------------------------#
       #  Merge diagnosis code variables with N-Code reference table to obtain severity  #
       #  and ISS body region variables for each diagnosis code and add them to the data #
       #---------------------------------------------------------------------------------#
-
+      print("inserting sev and br columns")
       for(i in dx_nums){
           # create column name
           dx_name <- paste0(dx_pre, i)
@@ -135,11 +162,11 @@ cat_trauma <- function(df, dx_pre, calc_method = 1, icd10 = TRUE, conflict_resol
           # add row variable for sorting back to original order
           df_ss$n <- 1:NROW(df_ss)
 
-          # strip out decimal
-          df_ss[ , dx_name] <- apply(df_ss[ , dx_name, drop=F], 1, function(x) sub("\\.", "", x))
+          # strip out decimal and take only one or zero letters followed by numbers
+          df_ss[ , dx_name] <- stringr::str_extract(sub("\\.", "", df_ss[ , dx_name]), "^[A-Z]?[0-9]+")
 
           # merge iss variables
-          temp <- merge(df_ss, ntab_s1, by.x=dx_name, by.y="dx", all.x=T, all.y=F, sort=F)
+          temp <- merge(df_ss, ntab, by.x=dx_name, by.y="dx", all.x=T, all.y=F, sort=F)
 
           # reorder rows after merge
           temp <- temp[order(temp$n), ]
@@ -156,32 +183,28 @@ cat_trauma <- function(df, dx_pre, calc_method = 1, icd10 = TRUE, conflict_resol
           names(temp) <- paste0(c("sev_","issbr_"), i)
 
           # add temp columns to dataframe
+          print(paste('inserting columns for ', dx_name))
           df <- .insert_columns(df, dx_name, temp)
 
       }
 
-      # Increment value in the temporary variable unk_unk if severity equals 9
-      # and ISS body region equals 9. (9 indicates unknown. This is not a great coding choice. Might change it to NA?)
-      # STATA code: replace unk_unk = unk_unk + 1 if `sev' == 9 & `issbr' == 9
-      for(i in dx_nums){
-            df$unk_unk <- df$unk_unk + (df[ , paste0("sev_", i)] == 9 & df[ , paste0("issbr_", i)] == 9)
-      }
-      # why do we care about unk_unk
-
-
       #----------------------------------------------------#
       # Create variables for maximum AIS/ISS body region.  #
       #----------------------------------------------------#
-# i=1
-      # for each body region 1-6 loop through dx codes and get max ais for that body region
-      for(i in 1:6){
+      # i=1
+      print("calc max ais for each body region")
+      # body regions are coded as text
+      body_regions <- unique(i10_map_emp$issbr)
+      # make usable for column names
+      issbr_names <- gsub("/", "_", body_regions)
+
+      # for each of the 6 body regions loop through dx codes and get max ais for that body region
+      for(i in body_regions){
             # Get severity columns and multiply by 1 if they are for body region i and 0 otherwise
             # This uses element-wise multiplication of matricies
-            #        all severity columns as a matrix      *    Indicator matrix of body region columns (entries 1 or 0)
-            # df[ , grepl("sev_|issbr_", names(df)), drop=F]
+            # all severity columns as a matrix * Indicator matrix of body region columns (entries 1 or 0)
             temp <- df[ , grepl("sev_", names(df)), drop=F] * (1*(df[ , grepl("issbr_", names(df))] == i))
 
-            # class(temp) # data.frame
             # convert all zeros to NA. zeros represent severity values not associated with body region i
             # temp <- data.frame(t(apply(temp, 1, function(x) ifelse(x==0, NA, x))))
 
@@ -189,9 +212,7 @@ cat_trauma <- function(df, dx_pre, calc_method = 1, icd10 = TRUE, conflict_resol
             # severity score of 9 implies unknown severity.
             # Thus we want to exclude these as long as there is at least one known severity for the body region
             # However if all severity scores for the body region are 9 then we will assign maxaisbr a value of 9
-            # (row=temp[1,,drop=F])
-            # class(row)
-            df[ , paste0("mxaisbr", i)] <- apply(temp, 1, function(row){
+            df[ , paste0("mxaisbr_", gsub("/","",i))] <- apply(temp, 1, function(row){
 
                   # convert all zeros to NA. zeros represent severity values not associated with body region i
                   row <- ifelse(row==0, NA, row)
@@ -225,9 +246,11 @@ cat_trauma <- function(df, dx_pre, calc_method = 1, icd10 = TRUE, conflict_resol
       # this is a bit complicated
       # if all maxbr_i are in (9,0,NA) then the max should be 9
       # if there is at least one positive maxbr_i that is not 9 then we need to exclude the 9s
+      # for each row in df...
       df$maxais <- apply(df, 1, function(row){
+            # select mxaisbr columns
             row <- row[grepl("mxaisbr", names(row))]
-            # if the max excluding 9 is zero then include 9 so that if there is a 9 the max will be 9
+            # if the max excluding 9 is zero then include 9 so that if there is a 9 then the max will be 9
             if(all(is.na(row))){
                   maxais <- as.numeric(NA)
             } else if(max(c9to0(row), na.rm = T) == 0){
@@ -239,36 +262,37 @@ cat_trauma <- function(df, dx_pre, calc_method = 1, icd10 = TRUE, conflict_resol
       })
       df$maxais <- as.numeric(df$maxais)
 
-      # there is more to do here for differences based on known/unknown severity ect... not sure yet
-      # time to figure this out
-
-
       #------------------------#
       #  Calculate ISS value.  #
       #------------------------#
 
-      # ISS is calculated as the sum of the squared three highest maxaisbr varaiables for a given person
-      # need to exclude 9s
+      # ISS is calculated as the sum of the squared three highest maxaisbr varaiables for a given person.
+      # We need to exclude 9s in this calculation.
+
       df$riss <- apply(df, 1, function(row){
 
-            #recode 9s as 0
-            row <- c9to0(row)
-
              # select the max ais variables for a given row
-            temp <- row[grepl("^mxaisbr[0-9]+$", names(row))]
+            temp <- row[grepl("^mxaisbr", names(row))]
 
             # for some reason apply is converting these to char. We will convert them back to numeric
-            temp <- as.numeric(temp)
+            # also convert 9s to 0
+            temp <- as.numeric(c9to0(temp))
 
             # Take the three highest, square them, and sum the result
-            sum(temp[order(-temp)[1:3]]^2) # what if there are only two?
+            # print(temp[order(-temp)[1:3]])
+            sum(temp[order(-temp)[1:3]]^2)
+            # what if there are only two?
+            # this code will sum the squares of the highest two
+            # is this what we want?
       })
 
-      # Replace ISS value with 75 if maximum severity is 6. Why?
+      # Replace ISS value with 75 if maximum severity is 6. This implies that the person is dead.
+      # 75 is the max ISS score 3(5^2) = 75
       df[df$maxais == 6,"riss"] <- 75
 
-      # Replace ISS value with 99 if maximum severity is 9. Why?
-      df[df$maxais == 9, "riss"] <- 99
+      # Replace ISS value with NA if maximum severity is 9.
+      # If maxais is 9 then it implies that there were only injuries of unknown severity
+      df[df$maxais == 9, "riss"] <- NA
 
 
       #---------------------------------------------------------------------#
@@ -281,12 +305,15 @@ cat_trauma <- function(df, dx_pre, calc_method = 1, icd10 = TRUE, conflict_resol
       dx_colnames <- paste0(dx_pre, 1:num_dx)
       ecode_colnames <- paste0("ecode_", 1:4)
 
-      #create e code columns
+      #create ecode columns
       df[ , ecode_colnames] <- NA
 
       # for each row extract the first 4 ecodes and add them to the e-code columns
-      # icd10 e-codes do not start with E. We need to get a list of all ecodes
-      ecodes <- etab_s1$dx
+      # icd10 e-codes do not start with E.
+
+      # get a list of all ecodes (includes icd10 code if requested)
+      ecodes <- etab$dx
+
       df[ , ecode_colnames] <- t(apply(df, 1, function(row){
             # remove trailing letters
             row <- sub("[A-Za-z]+$", "", row)
@@ -303,7 +330,7 @@ cat_trauma <- function(df, dx_pre, calc_method = 1, icd10 = TRUE, conflict_resol
             row_ecodes[1:4]
             # E_codes[!grepl("^E849", E_codes)][1:4]
       }))
-# i=2
+
       # loop through e codes and add associated variables from e code table
       for(i in 1:4){
           col_name <- paste("ecode_", i, sep="")
@@ -318,7 +345,7 @@ cat_trauma <- function(df, dx_pre, calc_method = 1, icd10 = TRUE, conflict_resol
           df_ss[,col_name] <- sub("\\.","", df_ss[,col_name])
 
           # merge in ecode variables
-          temp <- merge(df_ss, etab_s1, by.x=col_name, by.y="dx", all.x=T, all.y=F, sort=F)
+          temp <- merge(df_ss, etab, by.x=col_name, by.y="dx", all.x=T, all.y=F, sort=F)
 
           # reorder rows after merge
           temp <- temp[order(temp$n), ]
@@ -342,19 +369,13 @@ cat_trauma <- function(df, dx_pre, calc_method = 1, icd10 = TRUE, conflict_resol
       df[which(df$mechmaj1 %in% c(0, 4)), "bluntpen"] <- "P"
 
       # Why are we only considering the first ecode mechmaj here? convenience.
-      # It is a somewhat arbitrary decision. However it is unlikely that there would be more than 1 mechanism of injury
-      # - orignal STATA code below
-      # replace bluntpen = "B" if mechmaj1 == 2 | mechmaj1 == 5 | mechmaj1 == 6 | mechmaj1 == 7 | mechmaj1 == 8 |#
-      # # mechmaj1 == 9 | mechmaj1 == 13
-      # replace bluntpen = "P" if mechmaj1 == 0 | mechmaj1 == 4
+      # It is an arbitrary decision.
+      # However it is unlikely that there would be more than 1 mechanism of injury
 
       # set rownames
       rownames(df) <- 1:nrow(df)
 
-      # drop unk_unk . no sure why we need this variable. can we ditch it?
-      df <- subset(df, select = -unk_unk)
-
-      #return dataframe
+      # return dataframe
       df
 }
 
